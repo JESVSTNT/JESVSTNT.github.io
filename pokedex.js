72b336e5c560a792d9b76cbc9a2838055f044c21
JESVSTNT <jesusomarjuarez926@gmail.com>
2024-08-24 10:45:27 -0600

    Add files via upload

---
+++ pokedex.js
@@ -0,0 +1,83 @@
+const url="https://pokeapi.co/api/v2/pokemon/";
+const contenedor= document.getElementById('pokemon-container');
+
+
+const colores={
+             'grass': '#9eff4d', 
+             'fire': '#ffbc4d',
+             'rock': '#ada9a4',
+             'water': '#9adfe1',
+             'bug': '#34c639',
+             'normal': '#f4f1aa',
+             'psychic': '#b7b209',
+             'electric': '#fff700',
+             'fighting': '#f14e08',
+             'fairy': '#f4aaf0',
+             'poison': '#97008c',
+             'ghost': '#cf66c7',
+             'ice': '#329ed0',
+             'dragon': '#6793a8',
+             'dark': '#4d176c',
+             'ground': '#fffd91',
+};
+
+const obtenerPokemon= async (numeropokemon)=>{
+    let respuesta= await fetch(url+numeropokemon+"/");
+    let datos= await respuesta.json();
+    console.log(datos);
+    crearPokemon(datos);
+    
+};
+
+const generarNumeroAleatorio= (valorMaximo) =>{
+    let numero= Math.floor(Math.random()*valorMaximo);
+    return numero;
+};
+
+const crearPokemon= (datosPokemon) => {
+    let tipo= datosPokemon.types[0].type.name;
+    let  div = document.createElement('div');
+    let color= colores[tipo];
+    let template=`
+        <div class="card" style='background-color:${color};'>
+        <div class="front">
+          <h3>${datosPokemon.name}</h3>   
+          <img  class="pokemon"  src="${datosPokemon.sprites.other.home.front_default}"/>    
+        </div> 
+        <div class="back">    
+          <img  class="pokemon"  src="${datosPokemon.sprites.other.home.front_default}"/>
+          <p>Tipo: ${tipo}</p>
+        </div>           
+       </div>`;
+
+
+    div. innerHTML=template; 
+    contenedor.innerHTML="";
+    contenedor.appendChild(div);
+
+};
+
+// for(let i=1;i<=20;i++)
+{
+    // obtenerPokemon(i);
+}
+
+function buscadorpokemon(){
+  
+    var id_pokemon= document.getElementById("name_pokemon").value;
+    console.log(id_pokemon);
+    
+    if (id_pokemon == ""){
+        contenedor.innerHTML="";
+        // for (let i=1;i<=20;i++)
+        {
+            obtenerPokemon(i);
+
+        }
+    } else {
+        // contenedor.innerHTML = "";
+        obtenerPokemon(id_pokemon);
+    }
+};
+
+
